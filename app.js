abf5b29a1f98ace5419b4e3f7b539c3b91785d1a
Piyum Kaveeshwara <piyum7020@gmail.com>
2023-12-30 21:39:54 +0530

    modified:   app.js         modified:   package-lock.json         modified:   package.json         new file:   views/404.ejs         deleted:    views/404.html         new file:   views/about.ejs         deleted:    views/about.html         new file:   views/create.ejs         new file:   views/index.ejs         deleted:    views/index.html         new file:   views/partials/footer.ejs         new file:   views/partials/head.ejs         new file:   views/partials/nav.ejs

--- app.js
+++ app.js
@@ -3,25 +3,41 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 // listen for request
 app.listen(3000);
 
 app.get("/", (req, res) => {
   //   res.send("<p> home page 1 </p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
   //   res.send("<p> about page 1 </p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create-Blog" });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
