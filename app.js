caef6c4fbf5f3e505dd77ed0aeaa9246b1e47374
Piyum Kaveeshwara <piyum7020@gmail.com>
2024-01-01 11:51:31 +0530

    modified:   app.js         modified:   package-lock.json         modified:   package.json         new file:   public/styles.css         modified:   views/partials/head.ejs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 // express app
 const app = express();
@@ -8,6 +9,11 @@ app.set("view engine", "ejs");
 
 // listen for request
 app.listen(3000);
+
+// middleware & static files
+app.use(express.static("public"));
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   //   res.send("<p> home page 1 </p>");
