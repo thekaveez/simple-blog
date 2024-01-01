371937be8c01b56b405afe0ff33817428450baf4
Piyum Kaveeshwara <piyum7020@gmail.com>
2024-01-01 22:54:21 +0530

    modified:   app.js         modified:   public/styles.css         modified:   views/create.ejs         new file:   views/details.ejs         modified:   views/index.ejs

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set("view engine", "ejs");
 
 // middleware & static files
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
 
@@ -38,6 +39,38 @@ app.get("/blogs", (req, res) => {
 
     .then((result) => {
       res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
     })
     .catch((err) => console.log(err));
 });
